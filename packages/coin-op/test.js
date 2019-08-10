0bf6d878cf000d05d9b96e401893d91589208490
Mickey Burks <brks.mck@gmail.com>
2019-08-10 12:16:39 -0700

    Write tests for new API

--- packages/coin-op/test.js
+++ packages/coin-op/test.js
@@ -2,8 +2,9 @@ const assert = require('assert');
 const getSome = require('get-some');
 const {Machine, FSM} = require('./index.js');
 
-const [init, revenue, expenses] = getSome(() => FSM.createState());
+const [init, revenue, expenses, error] = getSome(() => FSM.createState());
 
+/*
 const steps = {
   [init]: {
     [revenue]() {
@@ -37,18 +38,44 @@ const steps = {
     },
   },
 };
+*/
 
-const machine = new Machine(steps, init);
+init.to(revenue, () => {
+  console.log('init to revenue')
+});
+
+revenue.to(init, () => {
+  console.log('revenue to init');
+});
+
+revenue.to(expenses, () => {
+  console.log('revenue to expenses');
+});
+
+expenses.onEnter(() => {
+  console.log('on enter called');
+});
+
+expenses.onLeave(() => {
+  console.log('on leave called');
+});
+
+expenses.to(revenue, () => {
+  console.log('expenses to revenue - fails');
+  return false;
+});
+
+const machine = new Machine(steps, {init, error});
 assertWithMessage(machine.state === init, 'machine starts in init');
 
-machine.transition(revenue)();
+machine.transition(revenue);
 assertWithMessage(machine.state === revenue, 'machine transitions to revenue');
 
-machine.transition(expenses)();
+machine.transition(expenses);
 assertWithMessage(machine.state === expenses, 'machine transitions to expenses');
 
-machine.transition(revenue)();
-assertWithMessage(machine.state === FSM.error, 'machine transitions to error instead of revenue');
+machine.transition(revenue);
+assertWithMessage(machine.state === error, 'machine transitions to error instead of revenue');
 
 function assertWithMessage(expectation, message) {
   try {
